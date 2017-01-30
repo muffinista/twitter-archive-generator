2e4a7668a88ad0a5454afe849f877e142fec2b54
Colin Mitchell <muffinista@gmail.com>
2017-01-30 13:02:12 -0500

    Adding upload code

--- index.js
+++ index.js
@@ -7,10 +7,73 @@ var Twit = require('twit');
 var conf = JSON.parse(fs.readFileSync('conf.json'));
 const LIST_ID = conf.list_id;
 
+var hasUpdates = {};
 
 var writeTweet = require('./writeTweet');
 
 var T = new Twit(conf.twitter);
+
+var AWS = require('aws-sdk');
+var ep = new AWS.Endpoint('s3.us.archive.org');
+
+var s3 = new AWS.S3({
+  endpoint: ep,
+  accessKeyId: conf.s3.accessKeyId,
+  secretAccessKey: conf.s3.secretAccessKey
+});
+
+
+var uploadToArchive = function(handle, ext) {
+  var src = "data/" + handle + "." + ext;
+
+  var fileStream = fs.createReadStream(src);
+  var params = {
+    Bucket: conf.bucket,
+    Key: handle + "." + ext,
+    Body: fileStream,
+    Metadata: {
+      "x-archive-meta-title": "Twitter archive for " + handle,
+      "x-archive-meta-subject": handle
+    }
+  };
+  
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
+    else if (data) {
+      console.log("Upload Success", data.Location);
+      console.log(data);
+    }
+  });
+
+};
+
+var uploadArchive = function(name) {
+  s3.createBucket({Bucket: conf.bucket}, function(err, data) {
+
+    // we pretty much assume the only possible error here
+    // is that the bucket already exists. be careful!
+    if (err && err.code !== "BucketAlreadyExists") {
+      console.log(err);
+    }
+    else {
+      uploadToArchive(name, "json");
+      uploadToArchive(name, "csv");
+    }
+  });
+};
+
+
+
+var uploadChanges = function() {
+  console.log("look for changes");
+  Object.keys(hasUpdates).forEach(function(name) {
+    uploadArchive(name);
+  });
+};
+
+setInterval(uploadChanges, 1000*60*5);
 
 var watch = function(list) {
   console.log("WATCH", follow);
@@ -25,18 +88,16 @@ var watch = function(list) {
     console.log("connected!");
   });
   stream.on('tweet', function (tweet) {
+    // Tweets created by the user.
+    // Tweets which are retweeted by the user.
+    // Replies to any Tweet created by the user.
+    // Retweets of any Tweet created by the user.
+    // Manual replies, created without pressing a reply button (e.g. “@twitterapi I agree”).
     if ( _.includes(list, tweet.user.id_str) ) {
       console.log(tweet);
-
-      // Tweets created by the user.
-      // Tweets which are retweeted by the user.
-      // Replies to any Tweet created by the user.
-      // Retweets of any Tweet created by the user.
-      // Manual replies, created without pressing a reply button (e.g. “@twitterapi I agree”).
-      writeTweet(tweet, console.log);
-    }
-    else {
-
+      writeTweet(tweet, function() {
+        hasUpdates[tweet.user.screen_name] = 1;
+      });
     }
   });
 };
